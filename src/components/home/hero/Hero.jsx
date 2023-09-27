fb9d824b34730b5e2e92cd753f31a3368c4c4de8
Ajeibi <ajeibipikin@gmail.com>
2023-09-27 08:36:32 +0100

    #

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO ACADEMIA' title='Where Education Meets Innovation' />
+            <p>Explore our diverse range of courses, engage with passionate instructors, and connect with a global community of learners. Join us as we embark on a quest for knowledge, growth, and endless possibilities.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
